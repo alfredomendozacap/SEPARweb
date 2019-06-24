e004248bb1bea7682547d85410323ae0a302c770
Angel Mendoza <1930.kick@gmail.com>
2019-06-24 00:20:03 -0500

    Se agregó el archivo config.php para la BD

--- libs/DB.php
+++ libs/DB.php
@@ -1,15 +1,19 @@
 <?php
+include_once 'config.php';
 
 class DB extends PDO{
 
-	private $hostname = 'localhost';
-	private $dbname = 'cms_separ';
-	private $username = 'root';
-	private $password = '';
+	private $hostname = ENV['DB_SERVER'];
+	private $port     = ENV['DB_PORT'];
+	private $dbname   = ENV['DB_DATABASE'];
+	private $username = ENV['DB_USERNAME'];
+	private $password = ENV['DB_PASSWORD'];
 
 	function __construct(){
-		$dns = 'mysql:dbname='.$this->dbname.';host='.$this->hostname;
-		parent::__construct($dns,$this->username,$this->password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		$dns = "mysql:dbname=$this->dbname;host=$this->hostname;port=$this->port";
+		$options = [
+			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+		];
+		parent::__construct($dns ,$this->username, $this->password, $options);
 	}
 }
-
---
+++ libs/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('ENV', [
+    'DB_SERVER'   => 'localhost',
+    'DB_PORT'     => '3306',
+    'DB_DATABASE' => 'cms_separ',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => '',
+]);
